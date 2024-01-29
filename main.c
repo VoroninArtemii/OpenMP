be4afc770be008a8826f9db79e2ad3e6553108a9
Artemii Voronin <102426178+VoroninArtemii@users.noreply.github.com>
2024-01-30 00:47:10 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char** argv){
+	const int count = 10000000;
+	const int random_seed = 920214;
+	const int target = 1;
+	const int threads[] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int *array = 0;
+	int index = -1;
+	srand(random_seed);
+	array = (int*)malloc(count*sizeof(int));
+	for (int i = 0; i < count; i++){
+		array[i] = rand();
+	}
+	for (int j = 0; j < 8; j++){
+		int flag = 0;
+		double start = omp_get_wtime();
+		#pragma omp parallel num_threads(threads[j]) shared(array, count, target, flag) reduction(max: index)
+		{
+			#pragma omp for
+			for (int i = 0; i < count; i++){
+				if (array[i] == target || flag == 1){
+					index = i;
+					i = count;
+					flag = 1;
+				}
+			}
+		}
+		double end = omp_get_wtime();
+		printf("Time for %d processors: %lf\n", threads[j], end - start);
+	}
+	printf("Found occurence of %d at index %d;\n", target, index);
+	return 0;
+}
